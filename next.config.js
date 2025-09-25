2152489e90131ca7f3096b80cac4028edf445184
Dalmo <dalmomendonca@gmail.com>
2025-09-25 17:26:34 -0500

    Will this finally fix it?

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
-/** @type {import('next').NextConfig} */
+﻿/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
