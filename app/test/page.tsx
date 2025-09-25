c17fcb3d63cf1f3dec580808c938c11b0bf47c2d
Dalmo <dalmomendonca@gmail.com>
2025-09-25 09:45:34 -0500

    Fix Netlify configuration for SSR Next.js app

---
+++ app/test/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+export default function TestPage() {
+  return (
+    <div>
+      <h1>Test Page</h1>
+      <p>This is a test page to see if static assets are generated.</p>
+    </div>
+  );
+}
